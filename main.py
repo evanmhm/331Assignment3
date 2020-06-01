13882bd4a467b79b199be5550504ab961c4bbf5d
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2020-06-01 15:32:00 -0700

    it compiles now but idk

--- main.py
+++ main.py
@@ -30,8 +30,6 @@ def main():
     print(training)
     print(testing)
 
-    # Importing the dataset
-    imdb_data = pd.read_csv('IMDB.csv', delimiter=',')
 
     # this vectorizer will skip stop words
     vectorizer = CountVectorizer(
@@ -42,7 +40,7 @@ def main():
     )
 
     # fit the vectorizer on the text
-    vectorizer.fit(imdb_data['review'])
+    vectorizer.fit([i[0] for i in training])
 
     # get the vocabulary
     inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
@@ -51,13 +49,13 @@ def main():
     neg_text = []
     pos_count = 0
     neg_count = 0
-    for i, review in enumerate(imdb_data['review'][0:30000]):
-        if (labels[i] == "positive"):
-            for word in list(set(vocabulary) & set(clean_text(review).split())):
+    for review in training:
+        if (review[1] == 1):
+            for word in list(set(vocabulary) & set(clean_text(review[0]).split())):
                 pos_text.append(word)
             pos_count += 1
         else:
-            for word in list(set(vocabulary) & set(clean_text(review).split())):
+            for word in list(set(vocabulary) & set(clean_text(review[0]).split())):
                 neg_text.append(word)
             neg_count += 1
 
@@ -67,8 +65,8 @@ def main():
     prob_neg = neg_count/(pos_count+neg_count)
 
 
-    correct = naive_bayes(imdb_data['review'][30000:40000], pos_dict, neg_dict, pos_text, neg_text, prob_pos, prob_neg, labels, vocabulary, 1, 30000)
-    print("Testing Accuracy: ", correct/10000)
+    correct = naive_bayes([i[0] for i in training], pos_dict, neg_dict, pos_text, neg_text, prob_pos, prob_neg, [i[1] for i in training], vocabulary, 1, 0)
+    print("Testing Accuracy: ", correct)
 
     # predictions = naive_bayes_predict(imdb_data['review'][40000:50000], pos_dict, neg_dict, pos_text, neg_text, prob_pos, prob_neg, vocabulary, 1.0)
     # with open("test-predictions3.csv", 'w') as file:
@@ -85,7 +83,7 @@ def naive_bayes(reviews, pos_dict, neg_dict, pos_text, neg_text, prob_pos, prob_
             conditional_pos += math.log(((pos_dict[word]+alpha)/(len(pos_text)+len(vocabulary)*alpha)) * prob_pos)
             conditional_neg += math.log(((neg_dict[word]+alpha)/(len(neg_text)+len(vocabulary)*alpha)) * prob_neg)
 
-        if ((conditional_pos > conditional_neg and labels[i+offset] == "positive") or (conditional_pos < conditional_neg and labels[i+offset] == "negative")):
+        if ((conditional_pos > conditional_neg and labels[i+offset] == 1) or (conditional_pos < conditional_neg and labels[i+offset] == 0)):
             correct += 1
 
     return correct
@@ -107,6 +105,7 @@ def naive_bayes_predict(reviews, pos_dict, neg_dict, pos_text, neg_text, prob_po
     return predictions
 
 def clean_text(text):
+    print(text)
     # remove HTML tags
     text = re.sub(r'<.*?>', '', text)
 
